0c75d1aac7c8be169a480246eaaa00728953b5de
poiuyt0418 <poiuyt0418@hotmail.com>
2022-10-03 21:51:53 -0400

    functional gameish

---
+++ Assets/World_Exit.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class World_Exit : MonoBehaviour
+{
+    public TMP_Text objText;
+    public Player_Control player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        objText.text = "Requires 2 Hands to exit.";
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter()
+    {
+        if (player.CheckPart("arm") && player.CheckPart("body"))
+        {
+            objText.text = "You have escaped.";
+        }
+        else
+        {
+            if (player.CheckPart("arm"))
+            {
+                objText.text = "1 Hand remaining.";
+            }
+            if (player.CheckPart("body"))
+            {
+                objText.text = "1 Hand remaining.";
+            }
+        }
+    }
+}
