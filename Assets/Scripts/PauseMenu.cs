e1207237b48e6fef2a04b48e48e8cacef841e321
I5LAND-DON <89559847+I5LAND-DON@users.noreply.github.com>
2022-10-15 21:36:54 -0400

    Added Pause Menu
    Pause menu has been implemented but still needs work

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using System.Threading;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+
+   public void Pause()
+   {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+   }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void Home(int sceneID)
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(sceneID);
+    }
+    
+
+}
