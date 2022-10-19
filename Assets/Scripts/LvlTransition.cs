57408bc4b0ba42478ede2190b7f869d84b914d0a
I5LAND-DON <89559847+I5LAND-DON@users.noreply.github.com>
2022-10-18 21:37:14 -0400

    Added Transition Level
    Added Transition Level

---
+++ Assets/Scripts/LvlTransition.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LvlTransition : MonoBehaviour
+{
+
+    public void PlayLevel ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+
+    public void PlaySecond ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+    public void PlayThird ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+    }
+}
