275c452881d4d8fa9d2b7f04a60dabcf97b4551a
I5LAND-DON <89559847+I5LAND-DON@users.noreply.github.com>
2022-11-08 23:49:38 -0500

    Created Game Mode Selection
    Game mode selection has been implemented

---
+++ Assets/Scripts/GameModeSelect.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameModeSelect : MonoBehaviour
+{
+
+    public void PlayNormal()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+
+    public void PlayEndless()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 5);
+    }
+
+    public void PlayThird()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+    }
+}
