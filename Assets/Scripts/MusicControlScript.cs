e3446165ccc860bb3930c61fa72dc61274686aa2
I5LAND-DON <89559847+I5LAND-DON@users.noreply.github.com>
2022-11-13 19:18:31 -0500

    Background music added
    Added background music and audio files

---
+++ Assets/Scripts/MusicControlScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+public class MusicControlScript : MonoBehaviour
+{
+   public static MusicControlScript instance;
+
+        private void Awake()
+
+    {
+        DontDestroyOnLoad(this.gameObject);
+
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
