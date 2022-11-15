c15ab00f7a97375888a8dcfa5564c12032d885b8
poiuyt0418 <poiuyt0418@hotmail.com>
2022-11-14 23:42:14 -0500

    Update ChessBoard.cs

--- Assets/Scripts/Chess/ChessBoard.cs
+++ Assets/Scripts/Chess/ChessBoard.cs
@@ -125,7 +125,6 @@ public class ChessBoard : MonoBehaviour
         Debug.Log("a");
         if (solved)
         {
-            Destroy(canvas);
             ButtonOff();
             return;
         }
